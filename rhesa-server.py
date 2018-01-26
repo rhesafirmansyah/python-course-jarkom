80f5bfac9232190cfac06154e2f610087cfd2163
rhesa firmansyah <35840972+rhesafirmansyah@users.noreply.github.com>
2018-01-26 21:53:25 +0700

    Add files via upload
    Tugas CP2 Jaringan dan Komunikasi
    Moch. Rhesa Firmansyah (04116008_SK-B)

---
+++ rhesa-server.py
@@ -0,0 +1,35 @@
+import socket
+import threading
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='(%(threadName)-10s) %(message)s',
+                    )
+
+def handle_client(sock_client, addr):
+    logging.debug('client connection accepted: {}'.format(addr))
+    message = ''
+    while message.find('bye') == -1:
+        message = sock_client.recv(1024)
+        hasil=eval(message)
+        hasil = 'Jawabannya adalah: {}'.format(hasil)
+        sock_client.send(hasil)
+    sock_client.close()
+
+sock_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock_server.bind(('127.0.0.1',9999))
+sock_server.listen(5)
+clients = []
+while True:
+    try:
+        s, a = sock_server.accept()
+        t_client = threading.Thread(target=handle_client, args=(s,a))
+        t_client.start()
+        t_client.join(1)
+        clients.append(t_client)
+    except Exception as e:
+        logging.debug(e)
+        break
+    except KeyboardInterrupt as k:
+        break
